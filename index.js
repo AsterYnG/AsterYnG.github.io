c209f5446712c316dd4b0de15d973843800b2224
AsterYnG <khudyakov.03@bk.ru>
2023-03-21 19:52:31 +0300

    2

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 "use strict";
 
-alert(location.href); // показывает текущий URL
 if (confirm("Перейти на Wikipedia?")) {
   location.href = "https://www.xvideos.com/tags/xxx"; // перенаправляет браузер на другой URL
 }
